3c7448d79c592b09bedc29450869838901e4e240
AksharPrajapati <aksharpp369@gmail.com>
2022-06-29 23:17:05 +0530

    added akira model with animation

--- src/Components/Canvas3D.js
+++ src/Components/Canvas3D.js
@@ -1,21 +1,52 @@
 import React, { useRef, useEffect, Suspense } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { OrbitControls, softShadows, useGLTF } from '@react-three/drei'
+import { OrbitControls, softShadows, useGLTF, useHelper } from '@react-three/drei'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { EffectComposer, Glitch, Scanline } from '@react-three/postprocessing'
 import { GlitchMode } from 'postprocessing'
+import { SpotLightHelper } from 'three'
+import * as THREE from 'three'
 
 gsap.registerPlugin(ScrollTrigger)
 function Thing({ anim, animSphere }) {
-  const { nodes, materials, scene } = useGLTF('/shoes.glb')
-  scene.scale.set(5, 5, 5)
+  const { nodes, materials, scene, animations } = useGLTF('/akira.glb')
+  scene.scale.set(13, 13, 13)
+  // scene.scale.set(5, 5, 5)
 
-  useFrame(() => {
+  // scene.rotation.set(0, 0, 0.1)
+  scene.position.y = -11.5
+  // scene.rotation.y = 0
+  scene.rotation.x = 1
+
+  let mixer
+  useEffect(() => {
+    if (animations.length) {
+      mixer = new THREE.AnimationMixer(scene)
+      const action = mixer.clipAction(animations[2])
+      const action1 = mixer.clipAction(animations[1])
+
+      action.play()
+      action1.play()
+      // animations.forEach((clip) => {
+      //   const action = mixer.clipAction(clip)
+      //   action.play()
+      // })
+    }
+  }, [animations])
+
+  useFrame((state, delta) => {
+    mixer?.update(delta)
     scene.rotation.y = -1 * anim.y
     // scene.scale.x = -1 * anim.x
-    // scene.scale.z = -1 * anim.x
   })
+
+  // useFrame(() => {
+  //   // console.log(anim.y * -1)
+  //   scene.rotation.y = -1 * anim.y
+  //   // scene.scale.x = -1 * anim.x
+  //   // scene.scale.z = -1 * anim.x
+  // })
   return <primitive object={scene} />
 }
 
@@ -38,7 +69,7 @@ function Effects() {
 function Canvas3D({ mainRef }) {
   let animable = {
     x: -8,
-    y: -2
+    y: 0
   }
   let animableSphere = {
     y: 0
@@ -57,8 +88,16 @@ function Canvas3D({ mainRef }) {
       })
       tl1.to(animable, {
         x: 8,
-        y: 3
+        y: 2
       })
+      // gsap.to('.scaleDown', {
+      //   scale: 0.6667,
+      //   scrollTrigger: {
+      //     trigger: '.canvas',
+      //     pin: '.canvas',
+      //     scrub: true
+      //   }
+      // })
       // const tl3 = gsap.timeline({
       //   scrollTrigger: {
       //     trigger: '.section-two',
@@ -88,10 +127,23 @@ function Canvas3D({ mainRef }) {
     }
   }, [mainRef])
 
+  const Lights = () => {
+    const light = useRef()
+    // useHelper(light, SpotLightHelper, 'cyan')
+    return (
+      <group>
+        <ambientLight />
+        <spotLight ref={light} position={[50, 40, 40]} castShadow />
+      </group>
+    )
+  }
+
   return (
-    <Canvas shadowMap camera={{ position: [0, 0, 10] }} style={{ zIndex: '-1', backgroundColor: 'gray' }}>
-      <ambientLight intensity={0.3} />
-      <spotLight intensity={0.3} angle={0.1} penumbra={1} position={[5, 25, 20]} />
+    <Canvas shadowMap camera={{ position: [0, 0, 15] }} className="canvas" style={{ zIndex: '-1', backgroundColor: 'gray' }}>
+      {/* <ambientLight intensity={0.7} />
+      <spotLight intensity={0.3} angle={0.1} penumbra={1} position={[10, 25, 20]} /> */}
+      <Lights />
+      {/* <spotLightHelper /> */}
       <Thing anim={animable} animSphere={animableSphere} />
       <Effects />
     </Canvas>
